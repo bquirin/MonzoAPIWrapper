285b279cd6b673cfe34b69d82d42176e94fce8c7
Brandon Quirin <quirban@amazon.com>
2022-08-02 09:53:35 +0100

    Created test file for monzo api wrapper

---
+++ test_monzo_api_wrapper.py
@@ -0,0 +1,17 @@
+import unittest 
+import monzo_api_wrapper
+
+class TestMonzoAPIWrapper(unittest.TestCase):
+
+    def test_something():
+        pass
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
